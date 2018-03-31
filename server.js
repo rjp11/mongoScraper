307f8cf13c6dab3a0f9c8403f179cba19ee0e906
Ryan Parks <ryan.j.parks12@gmail.com>
2018-03-31 07:10:07 -0500

    removed summary from cheerio selector to function properly

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ var cheerio = require("cheerio");
 var app = express();
 
 // Database configuration
-var databaseUrl = "scraper";
-var collections = ["scrapedData"];
+var databaseUrl = "mongoScraper";
+var collections = ["ringerData"];
 
 // Hook mongojs configuration to the db variable
 var db = mongojs(databaseUrl, collections);
@@ -26,12 +26,17 @@ app.get("/scrape", function (req, res) {
   request("https://www.theringer.com/", function (error, response, html) {
     var $ = cheerio.load(html);
 
+    if (error){
+        console.log(error);
+        throw error;
+    }
+
     var results = [];
 
     $("h2.c-entry-box--compact__title").each(function (i, element) {
       var title = $(element).text();
       var link = $(element).children().attr("href");
-      var summary = $(element).
+      // var summary = $(element).
 
       results.push({
         title,
