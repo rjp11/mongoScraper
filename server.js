61fbe5a8a49f16c6d9c8044e6f06e290d463a115
Ryan Parks <ryan.j.parks12@gmail.com>
2018-03-31 06:47:59 -0500

    npm packages added. server.js updated based on 18.2 in class examples to contain a route that scrapes The Ringer for headlines and the link.

---
+++ server.js
@@ -0,0 +1,52 @@
+var express = require("express");
+var mongojs = require("mongojs");
+// Require request and cheerio. This makes the scraping possible
+var request = require("request");
+var cheerio = require("cheerio");
+
+// Initialize Express
+var app = express();
+
+// Database configuration
+var databaseUrl = "scraper";
+var collections = ["scrapedData"];
+
+// Hook mongojs configuration to the db variable
+var db = mongojs(databaseUrl, collections);
+db.on("error", function (error) {
+  console.log("Database Error:", error);
+});
+
+// Main route (simple Hello World Message)
+app.get("/", function (req, res) {
+  res.send("Hello world");
+});
+
+app.get("/scrape", function (req, res) {
+  request("https://www.theringer.com/", function (error, response, html) {
+    var $ = cheerio.load(html);
+
+    var results = [];
+
+    $("h2.c-entry-box--compact__title").each(function (i, element) {
+      var title = $(element).text();
+      var link = $(element).children().attr("href");
+      var summary = $(element).
+
+      results.push({
+        title,
+        link
+      });
+
+    });
+
+    db.scrapedData.insert(results);
+    res.json(results);
+  });
+});
+
+
+// Listen on port 3000
+app.listen(3000, function () {
+  console.log("App running on port 3000!");
+});
