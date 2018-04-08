142276a9c2b85927ac5e85afa136dcf789ce99f6
Ryan Parks <ryan.j.parks12@gmail.com>
2018-04-07 22:10:54 -0500

    file cleanup

--- server.js
+++ server.js
@@ -16,8 +16,6 @@ app.use(logger("dev"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-// Serve static content for the app from the "public" directory in the application directory.
-//app.use(express.static("public"));
 
 app.engine("handlebars", exprhbs({defaultLayout: "main"}));
 app.set("view engine", "handlebars");
@@ -27,13 +25,8 @@ var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoScraper"
 mongoose.connect(MONGODB_URI);
 
 
-// require("./routes/apiRoutes")(app);
-// require("./routes/htmlRoutes")(app);
 app.use(routes);
 
-
-
-// Listen on port 3000
 app.listen(PORT, function () {
     console.log("Listening on port %s", PORT);
 });
